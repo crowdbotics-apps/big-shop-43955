c288d1701f35c873976a9822ce40d4576250d77a
Crowdbotics <team@crowdbotics.com>
2023-10-15 23:07:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gig-shop-43955",
+  url: "https://gig-shop-43955.botics.co",
+  api: "https://gig-shop-43955.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
